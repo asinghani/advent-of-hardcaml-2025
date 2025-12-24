c6be9fffa15afebd06419c03e8d421985693366e
Anish Singhani <anish_oss@anishsinghani.com>
2025-12-23 22:27:22 -0500

    Back after a short hiatus, fix ability to run the build on macOS

--- inputs/embed.sh
+++ inputs/embed.sh
@@ -3,8 +3,14 @@
 # Embed all of the inputs into one text file to allow embedding it into the
 # OCaml input parser binaries
 
+if [[ "$(uname)" == "Darwin" ]]; then
+    base64=gbase64
+else
+    base64=base64
+fi
+
 for file in *.txt ../examples_for_testing/inputs/*.txt; do
   fname=$(basename "$file")
-  b64=$(base64 -w 0 "$file")
+  b64=$($base64 -w 0 "$file")
   echo "$fname $b64"
 done
